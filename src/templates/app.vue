c70263bc7b46d0c54c6c2e65603e12e93082c51a
elf-mouse <ifmouz@gmail.com>
2016-12-13 13:02:13 +0800

    update tpl

--- src/templates/app.vue
+++ src/templates/app.vue
@@ -1,53 +1,84 @@
 <template>
   <div class="container">
-    <ui-tabs :tab="tabs.tab" :panel="tabs.panel">
-      <template slot="panel0">安顺地区覅的设计方案</template>
-      <template slot="panel2">asdfaosdjfoadjfa</template>
-    </ui-tabs>
-    <ui-badge :class="'test'" value="99+" noBackground overlap>
-      <b>Badge</b>
-    </ui-badge>
-    <ui-button primary @click="show">Primary Button</ui-button>
-    <ui-button effect @click="hide">Button</ui-button>
-    <ui-list :items="items2" threeLine>
-      <template slot="action" scope="props">
-        <ui-button effect @click.native="test(props.item)">点赞</ui-button>
+    <ui-layout noDesktopDrawerButton
+      :header="layout.header"
+      :drawer="layout.drawer">
+
+      <template slot="header-right" scope="props">
+        <ui-menu :data="menus" :position="3" @clicked="onMenu" effect></ui-menu>
       </template>
-    </ui-list>
-    <ui-grid :data="grid"></ui-grid>
-    <ui-loading isActive></ui-loading>
-    <ui-table
-      :col="table.col"
-      :data="table.data"
-      :thead="table.thead"
-      :tbody="table.tbody"
-      :tfoot="table.tfoot"
-      :action="table.action"
-      :selectable="table.selectable"
-      :checkList="table.checkList"
-      @edit="onEdit"
-      @delete="onDelete"
-      @selected="onSelected">
-    </ui-table>
-    <form>
-      <ui-textfield id="username" name="username" :model="formData.username" @input="changeText" pattern="-?[0-9]*(\.[0-9]+)?" label="用户名" error="请输入数字"></ui-textfield>
-      <div>
-        <p>复选框</p>
-        <ui-checkbox name="answer[]" value="1" :model="formData.answer" @input="chooseAnswer">选项A</ui-checkbox>
-        <ui-checkbox name="answer[]" value="2" :model="formData.answer" @input="chooseAnswer">选项B</ui-checkbox>
-        <ui-checkbox name="answer[]" value="3" :model="formData.answer" @input="chooseAnswer">选项C</ui-checkbox>
-        <p>单选框</p>
-        <ui-radio name="sex" value="F" :model="formData.gender" @input="chooseSex">女</ui-radio>
-        <ui-radio name="sex" value="M" :model="formData.gender" @input="chooseSex">男</ui-radio>
-        <p>图标</p>
-        <ui-icon name="options[]" value="a" :model="formData.options" @input="chooseIcon">X</ui-icon>
-        <ui-icon name="options[]" value="c" :model="formData.options" @input="chooseIcon">Y</ui-icon>
-        <ui-icon name="options[]" value="b" :model="formData.options" @input="chooseIcon">Z</ui-icon>
-        <p>开关</p>
-        <ui-switch name="switch1" :model="formData.switch1" @input="changeSwitch">On/Off</ui-switch>
+
+      <div slot="content">
+        <ui-tabs :tab="tabs.tab" :panel="tabs.panel" effect>
+          <template slot="panel0">安顺地区覅的设计方案</template>
+          <template slot="panel2">asdfaosdjfoadjfa</template>
+        </ui-tabs>
+
+        <ui-tooltip :target="target" tips="请输入"></ui-tooltip>
+
+        <ui-chip value="奥数都发局速读法"></ui-chip>
+
+        <ui-slider :max="100" :model="sliderValue" @input="changeSlider"></ui-slider>
+
+        <hr>
+        <br>
+
+        <ui-card :dp="2">
+        </ui-card>
+
+        <br>
+        <hr>
+
+        <ui-badge :class="'test'" value="99+" noBackground overlap>
+          <b>Badge</b>
+        </ui-badge>
+        <ui-button primary @click.native="show">Primary Button</ui-button>
+        <ui-button effect @click.native="hide">Button</ui-button>
+        <ui-list :items="items2" threeLine>
+          <template slot="action" scope="props">
+            <ui-button effect @click.native="test(props.item)">点赞</ui-button>
+          </template>
+        </ui-list>
+        <ui-grid :data="grid"></ui-grid>
+        <ui-loading isActive></ui-loading>
+        <ui-table
+          :col="table.col"
+          :data="table.data"
+          :thead="table.thead"
+          :tbody="table.tbody"
+          :tfoot="table.tfoot"
+          :action="table.action"
+          :selectable="table.selectable"
+          :checkList="table.checkList"
+          @edit="onEdit"
+          @delete="onDelete"
+          @on-selected="onSelected">
+        </ui-table>
+        <form>
+          <ui-textfield id="username" name="username" :model="formData.username" @input="changeText" pattern="-?[0-9]*(\.[0-9]+)?" label="用户名" error="请输入数字"></ui-textfield>
+          <div>
+            <p>复选框</p>
+            <ui-checkbox name="answer[]" value="1" :model="formData.answer" @input="chooseAnswer">选项A</ui-checkbox>
+            <ui-checkbox name="answer[]" value="2" :model="formData.answer" @input="chooseAnswer">选项B</ui-checkbox>
+            <ui-checkbox name="answer[]" value="3" :model="formData.answer" @input="chooseAnswer">选项C</ui-checkbox>
+            <p>单选框</p>
+            <ui-radio name="sex" value="F" :model="formData.gender" @input="chooseSex">女</ui-radio>
+            <ui-radio name="sex" value="M" :model="formData.gender" @input="chooseSex">男</ui-radio>
+            <p>图标</p>
+            <ui-icon name="options[]" value="a" :model="formData.options" @input="chooseIcon">X</ui-icon>
+            <ui-icon name="options[]" value="c" :model="formData.options" @input="chooseIcon">Y</ui-icon>
+            <ui-icon name="options[]" value="b" :model="formData.options" @input="chooseIcon">Z</ui-icon>
+            <p>开关</p>
+            <ui-switch name="switch1" :model="formData.switch1" @input="changeSwitch">On/Off</ui-switch>
+          </div>
+          <ui-button primary @click.native="submit">测试按钮</ui-button>
+        </form>
+
+        <ui-snackbar :message="popup.message" :active="popup.active" @done="hide"></ui-snackbar>
+
       </div>
-      <ui-button primary @click.native="submit">测试按钮</ui-button>
-    </form>
+
+    </ui-layout>
   </div>
 </template>
 
@@ -55,6 +86,61 @@
 export default {
   data() {
     return {
+      popup: {
+        message: 'Hello WOrld',
+        active: false
+      },
+      sliderValue: 25,
+      target: '<i class="icon">icon</i>',
+      menus: [{
+            url:'/a',
+            value: 'aaa'
+          },{
+            url:'/b',
+            value: 'bbb',
+            disabled: true
+          },{
+            url:'/c',
+            value: 'ccc'
+          }],
+      layout: {
+        header: {
+          title: '管理后台系统',
+          left: [{
+            url:'/a',
+            value: 'aaa'
+          },{
+            url:'/b',
+            value: 'bbb'
+          },{
+            url:'/c',
+            value: 'ccc'
+          }],
+          right: [{
+            url:'/a',
+            value: 'aaa'
+          },{
+            url:'/b',
+            value: 'bbb'
+          },{
+            url:'/c',
+            value: 'ccc'
+          }]
+        },
+        drawer: {
+          title: '管理后台系统',
+          links: [{
+            url:'/a',
+            value: 'aaa'
+          },{
+            url:'/b',
+            value: 'bbb'
+          },{
+            url:'/c',
+            value: 'ccc'
+          }]
+        }
+      },
       grid: [
       [2,{
         col: 4
@@ -166,6 +252,12 @@ export default {
     };
   },
   methods: {
+    changeSlider(data) {
+      this.sliderValue = data;
+    },
+    onMenu(data) {
+      console.log('menu', data);
+    },
     onShare(data) {
       console.log('share', data);
     },
@@ -185,10 +277,13 @@ export default {
       console.log('change', data);
     },
     show() {
-      UiDialog.close();
+      console.log('show popup');
+      this.popup.message = (new Date()).toString();
+      this.popup.active = true;
     },
     hide() {
-      UiDialog.hide();
+      console.log('hide');
+      this.popup.active = false;
     },
     test(id) {
       console.log(id);
@@ -213,20 +308,8 @@ export default {
     }
   },
   created() {
-    // UiDialog.alert({
-    //   title: '弹出框标题',
-    //   message: '弹出框消息',
-    //   callback: () => {
-    //     console.log('返回值');
-    //   }
-    // });
   }
 };
-
-// <ui-dialog>
-//       <span slot="title">标题</span>
-//       <p slot="content">内容</p>
-//     </ui-dialog>
 </script>
 
 <style>

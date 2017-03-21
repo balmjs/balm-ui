0302e031ae3de78cb88379b8050e8987f57bb783
elf-mouse <ifmouz@gmail.com>
2017-03-21 12:48:37 +0800

    update layout & pagination apidoc

--- docs/scripts/views/layout.vue
+++ docs/scripts/views/layout.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    // TODO
+  <div class="demo-list">
+    <div class="component-title">
+      <h3>Layout</h3>
+    </div>
+
+    <ui-apidoc name="layout"></ui-apidoc>
   </div>
 </template>

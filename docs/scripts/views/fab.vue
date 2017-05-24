89ca6559b32e9cb008750181a89badfb72678213
elf-mouse <ifmouz@gmail.com>
2017-05-24 14:34:39 +0800

    update

--- docs/scripts/views/fab.vue
+++ docs/scripts/views/fab.vue
@@ -37,16 +37,16 @@
 
       <fieldset>
         <legend>Custom FABs</legend>
-        <ui-fab cssOnly>
+        <ui-fab>
           <i class="fa fa-flag fa-lg" aria-hidden="true"></i>
         </ui-fab>
-        <ui-fab cssOnly plain>
+        <ui-fab plain>
           <i class="fa fa-smile-o fa-lg" aria-hidden="true"></i>
         </ui-fab>
-        <ui-fab cssOnly dark>
+        <ui-fab>
           <i class="fa fa-camera-retro fa-lg"></i>
         </ui-fab>
-        <ui-fab cssOnly plain dark>
+        <ui-fab plain>
           <i class="fa fa-spinner fa-pulse fa-lg fa-fw"></i>
         </ui-fab>
       </fieldset>

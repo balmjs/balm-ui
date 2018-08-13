9bf653305e429526a023fb6120446e1f2c266981
Elf-mousE <ifmouz@gmail.com>
2018-08-13 23:03:16 +0800

    0.6.0 beta.28

--- src/scripts/components/tabs.js
+++ src/scripts/components/tabs.js
@@ -1,16 +1,18 @@
 import multiBootstrap from '../config/multi-bootstrap';
 import UiTabs from './tabs/tabs';
-import UiTabBar from './tabs/tab-bar';
-import UiTabBarScroller from './tabs/tab-bar-scroller';
 import UiTab from './tabs/tab';
+import UiTabBar from './tabs/tab-bar';
+import UiTabIndicator from './tabs/tab-indicator';
+import UiTabScroller from './tabs/tab-scroller';
 import UiPanels from './tabs/panels';
 import UiPanel from './tabs/panel';
 
 export default multiBootstrap({
   UiTabs,
-  UiTabBar,
-  UiTabBarScroller,
   UiTab,
+  UiTabBar,
+  UiTabIndicator,
+  UiTabScroller,
   UiPanels,
   UiPanel
 });

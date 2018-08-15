cc698b2651539ee8b7fd5e7c5f59176d3aad95b6
Elf-mousE <ifmouz@gmail.com>
2018-08-15 09:46:35 +0800

    update docs for killing ie

---
+++ docs/scripts/kill-ie.js
@@ -0,0 +1,31 @@
+import {
+  detectIE
+} from '../../src/scripts/plus'; // Default Usage
+
+const IE = detectIE();
+
+const isIE = IE && IE < 11; // Browser support IE 11+
+
+const killIE = () => {
+  let body = document.getElementsByTagName('body')[0];
+  let template = `<div class="kill-ie">
+      <div class="container">
+        <div class="content">
+          <h1>您的浏览器版本过低，请点击<a href="https://browsehappy.com/">链接</a>升级您的浏览器</h1>
+          <p>
+            PS：推荐使用<a href="https://www.google.cn/intl/zh-CN/chrome/">谷歌浏览器</a>；如果是双核浏览器，请切换至<b>急速模式</b>
+          </p>
+        </div>
+      </div>
+    </div>`;
+
+  document.getElementsByTagName('html')[0].style.height = '100%';
+  body.style.height = '100%';
+
+  body.innerHTML = template;
+};
+
+export {
+  isIE,
+  killIE
+};
--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,7 +1,9 @@
+import {
+  isIE,
+  killIE
+} from './kill-ie';
 import './polyfill';
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueMeta from 'vue-meta';
 // import VueI18n from 'vue-i18n';
 import $http from './plugins/$http';
 // import $bus from './plugins/$bus';
@@ -22,73 +24,77 @@ import prismjs from 'prismjs';
 // ready translated locales
 // import { locales } from './config/lang';
 
-const DEBUG = process.env.NODE_ENV === 'production' ? false : true;
+if (isIE) {
+  killIE();
+} else {
+  const DEBUG = process.env.NODE_ENV === 'production' ? false : true;
 
-Vue.config.productionTip = false;
-Vue.prototype.DEBUG = DEBUG;
-// Vue.use(VueI18n);
-Vue.use($http);
-// Vue.use($bus);
-// Vue.use($store);
-Vue.use(BalmUI);
-Vue.use(BalmUIPlus, {
-  validator: validatorRules
-});
-Vue.use(BalmUIMigrate);
+  Vue.config.productionTip = false;
+  Vue.prototype.DEBUG = DEBUG;
+  // Vue.use(VueI18n);
+  Vue.use($http);
+  // Vue.use($bus);
+  // Vue.use($store);
+  Vue.use(BalmUI);
+  Vue.use(BalmUIPlus, {
+    validator: validatorRules
+  });
+  Vue.use(BalmUIMigrate);
 
-Vue.component(UiMarkdown.name, UiMarkdown);
-Vue.component(UiApidocs.name, UiApidocs);
-Vue.component(UiLogo.name, UiLogo);
-Vue.component(UiAccordion.name, UiAccordion);
-Vue.component(UiFooterNav.name, UiFooterNav);
+  Vue.component(UiMarkdown.name, UiMarkdown);
+  Vue.component(UiApidocs.name, UiApidocs);
+  Vue.component(UiLogo.name, UiLogo);
+  Vue.component(UiAccordion.name, UiAccordion);
+  Vue.component(UiFooterNav.name, UiFooterNav);
 
-Vue.prototype.$prism = prismjs;
-Vue.prototype.$docs = {
-  props: {
-    thead: ['Name', 'Type', 'Default', 'Description'],
-    tbody: [
-      'name',
-      'type',
-      'default',
-      {
-        field: 'description',
-        raw: true
-      }
-    ]
-  },
-  slots: {
-    thead: ['Name', 'Description', 'Slot'],
-    tbody: ['name', 'description', 'props']
-  },
-  events: {
-    thead: ['Name', 'Type', 'Description'],
-    tbody: ['name', 'type', 'description']
-  },
-  sass: {
-    thead: ['Variable', 'Description'],
-    tbody: [
-      'var',
-      {
-        field: 'description',
-        raw: true
-      }
-    ]
-  }
-};
-Vue.prototype.$domain = DEBUG ? '' : '//material.balmjs.com';
+  Vue.prototype.$prism = prismjs;
+  Vue.prototype.$docs = {
+    props: {
+      thead: ['Name', 'Type', 'Default', 'Description'],
+      tbody: [
+        'name',
+        'type',
+        'default',
+        {
+          field: 'description',
+          raw: true
+        }
+      ]
+    },
+    slots: {
+      thead: ['Name', 'Description', 'Slot'],
+      tbody: ['name', 'description', 'props']
+    },
+    events: {
+      thead: ['Name', 'Type', 'Description'],
+      tbody: ['name', 'type', 'description']
+    },
+    sass: {
+      thead: ['Variable', 'Description'],
+      tbody: [
+        'var',
+        {
+          field: 'description',
+          raw: true
+        }
+      ]
+    }
+  };
+  Vue.prototype.$domain = DEBUG ? '' : '//material.balmjs.com';
 
-// Create VueI18n instance with options
-// const i18n = new VueI18n({
-//   locale: 'en', // set locale
-//   messages: locales // set locale messages
-// });
+  // Create VueI18n instance with options
+  // const i18n = new VueI18n({
+  //   locale: 'en', // set locale
+  //   messages: locales // set locale messages
+  // });
 
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: {
-    App
-  }
-  // i18n
-});
+  new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {
+      App
+    }
+    // i18n
+  });
+}

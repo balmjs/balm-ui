60b03323c38ab81e1b72761f02d8b416d5a8fd44
Elf-mousE <ifmouz@gmail.com>
2018-08-22 20:37:10 +0800

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,19 @@ module.exports = {
     mocha: true
   },
   // required to lint *.vue files
-  plugins: ['vue'],
+  plugins: ['vue', 'prettier'],
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential'],
+  extends: ['plugin:vue/essential', 'plugin:prettier/recommended'],
   // add your custom rules here
   rules: {
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true
+      }
+    ]
   }
 };

8c41b8a5392e5813efa397ffd856e6342fbee41f
Elf-mousE <ifmouz@gmail.com>
2018-09-04 11:38:33 +0800

    update test config

--- babel.config.js
+++ babel.config.js
@@ -34,31 +34,7 @@ module.exports = {
   ],
   env: {
     test: {
-      presets: ['@babel/preset-env'],
-      plugins: [
-        // Stage 2
-        [
-          '@babel/plugin-proposal-decorators',
-          {
-            legacy: true
-          }
-        ],
-        '@babel/plugin-proposal-function-sent',
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-numeric-separator',
-        '@babel/plugin-proposal-throw-expressions',
-        // Stage 3
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-syntax-import-meta',
-        [
-          '@babel/plugin-proposal-class-properties',
-          {
-            loose: false
-          }
-        ],
-        '@babel/plugin-proposal-json-strings',
-        'istanbul'
-      ]
+      plugins: ['istanbul']
     }
   }
 };

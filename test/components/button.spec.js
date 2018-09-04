74a2466ee077bd8fa4e42a49325018753c40a4d0
Elf-mousE <ifmouz@gmail.com>
2018-09-04 14:17:44 +0800

    update test for button

--- test/components/button.spec.js
+++ test/components/button.spec.js
@@ -2,7 +2,83 @@ import UiButton from '@/components/button/button.vue';
 
 describe('<ui-button>', () => {
   it('renders default button', () => {
-    const wrapper = shallowMount(UiButton);
-    expect(wrapper.findAll('button')).toHaveLength(1);
+    const defaultText = 'Text Button';
+
+    const wrapper = shallowMount(UiButton, {
+      slots: {
+        default: defaultText
+      }
+    });
+
+    if (DEBUG) {
+      console.log('[OUTPUT]:', wrapper.html());
+      console.log();
+    }
+
+    expect(wrapper.is('button')).toBe(true);
+    expect(wrapper.text()).toBe(defaultText);
   });
+});
+
+describe('<ui-button raised>', () => {
+  it('renders raised button', () => {
+    const defaultText = 'Raised Button';
+
+    const wrapper = shallowMount(UiButton, {
+      slots: {
+        default: defaultText
+      },
+      propsData: {
+        raised: true
+      }
+    });
+
+    if (DEBUG) {
+      console.log('[OUTPUT]:', wrapper.html());
+      console.log();
+    }
+
+    expect(wrapper.is('button')).toBe(true);
+    expect(wrapper.text()).toBe(defaultText);
+    expect(wrapper.classes()).toContain('mdc-button--raised');
+  });
+});
+
+describe('<ui-button icon>', () => {
+  it('renders button with icon', () => {
+    const defaultText = 'Icon Button';
+    const icon = 'favorite';
+
+    const wrapper = shallowMount(UiButton, {
+      slots: {
+        default: defaultText
+      },
+      propsData: {
+        icon
+      }
+    });
+
+    if (DEBUG) {
+      console.log('[OUTPUT]:', wrapper.html());
+      console.log();
+    }
+
+    expect(wrapper.is('button')).toBe(true);
+    expect(wrapper.find('.mdc-button__icon').text()).toBe(icon);
+  });
+});
+
+describe('<ui-button @click>', () => {
+  it('Click on button calls our method', () => {
+    const spy = sinon.spy();
+
+    const wrapper = mount(UiButton, {
+      methods: {
+        handleClick: spy
+      }
+    });
+    wrapper.trigger('click');
+
+    expect(spy.calledOnce).toBe(true);
+  })
 });
--- test/setup.js
+++ test/setup.js
@@ -11,7 +11,10 @@ const {
 // const { render, renderToString } = require('@vue/server-test-utils');
 
 // make available globally
+global.DEBUG = false;
 global.expect = require('expect');
+global.mount = mount;
 global.shallowMount = shallowMount;
 // global.render = render;
 // global.renderToString = renderToString;
+global.sinon = require('sinon');

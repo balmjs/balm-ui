637a96a5f17e5256e9e34ca456da9349ced83bf5
Elf-mousE <ifmouz@gmail.com>
2018-10-26 12:45:44 +0800

    update new chip

--- src/scripts/components/chips/chip.vue
+++ src/scripts/components/chips/chip.vue
@@ -1,11 +1,66 @@
 <template>
+  <!-- Container -->
   <div class="mdc-chip" @click="$parent.handleClick">
-    <slot></slot>
+    <!-- Thumbnail (optional) -->
+    <slot name="thumbnail" :className="thumbnailClass">
+      <i :class="thumbnailClass">{{ thumbnail }}</i>
+    </slot>
+    <!-- Text -->
+    <div class="mdc-chip__text">
+      <slot></slot>
+    </div>
+    <!-- Remove icon [optional] -->
+    <slot name="remove-icon" :className="removeIconClass">
+      <i :class="removeIconClass"
+        tabindex="0"
+        role="button"
+        @click="handleClick">
+        {{ removeIcon }}
+      </i>
+    </slot>
   </div>
 </template>
 
 <script>
+import UI_GLOBAL from '../../config/constants';
+
+// Define constants
+const UI_CHIP = {
+  EVENT: {
+    CLICK: 'click'
+  }
+};
+
 export default {
-  name: 'ui-chip'
+  name: 'ui-chip',
+  props: {
+    thumbnail: String,
+    removeIcon: {
+      type: String,
+      default: 'cancel'
+    }
+  },
+  data() {
+    return {
+      UI_GLOBAL
+    };
+  },
+  computed: {
+    thumbnailClassName() {
+      return [
+        UI_GLOBAL.mdi,
+        'mdc-chip__icon mdc-chip__icon--leading',
+        { 'mdc-chip__icon--leading-hidden': this.hidden }
+      ];
+    },
+    removeIconClass() {
+      return [UI_GLOBAL.mdi, 'mdc-chip__icon mdc-chip__icon--trailing'];
+    }
+  },
+  methods: {
+    handleClick(event) {
+      this.$emit(UI_CHIP.EVENT.CLICK, event);
+    }
+  }
 };
 </script>

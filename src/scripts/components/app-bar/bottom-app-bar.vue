d124a262106017f034b2ba9797543d15acbefdf6
Elf-mousE <ifmouz@gmail.com>
2019-04-03 18:14:00 +0800

    update

--- src/scripts/components/app-bar/bottom-app-bar.vue
+++ src/scripts/components/app-bar/bottom-app-bar.vue
@@ -1,9 +1,22 @@
 <template>
-  <!-- TODO -->
-  <div></div>
+  <!-- Container -->
+  <footer class="mdc-bottom-app-bar">
+    <div class="mdc-bottom-app-bar__row">
+      <section class="mdc-bottom-app-bar__section mdc-bottom-app-bar__section--align-start">
+        <!-- Navigation drawer control -->
+      </section>
+      <section class="mdc-bottom-app-bar__section mdc-bottom-app-bar__section--align-center">
+        <!-- Floating action button (FAB) -->
+      </section>
+      <section class="mdc-bottom-app-bar__section mdc-bottom-app-bar__section--align-end">
+        <!-- Action icon / Overflow menu control -->
+      </section>
+    </div>
+  </footer>
 </template>
 
 <script>
+// TODO
 export default {
   name: 'ui-bottom-app-bar'
 };

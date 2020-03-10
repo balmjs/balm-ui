5eae5df5c40244f0ab831b039ea9b58893525790
Elf-mousE <ifmouz@gmail.com>
2020-03-10 18:09:39 +0800

    update event plugin

--- src/scripts/plugins/event.js
+++ src/scripts/plugins/event.js
@@ -19,25 +19,44 @@ const callback = fn => {
   return result;
 };
 
+function handleCustomEvent(properties, value, data = null) {
+  let key = properties.shift();
+  let currentValue = data ? data[key] : this[key];
+
+  if (properties.length) {
+    handleCustomEvent.call(this, properties, value, currentValue);
+  } else {
+    data[key] = value;
+  }
+}
+
+function handleEvent(_property, value) {
+  if (getType(new Function()) === 'function') {
+    new Function('value', `this.${_property} = value;`).call(this, value);
+  } else {
+    handleCustomEvent.call(this, _property.split('.'), value);
+  }
+}
+
 const EventMethods = {
   onChange(_property, value, fn = noop) {
-    new Function('value', `this.${_property} = value;`).call(this, value);
+    handleEvent.call(this, _property, value);
     return callback(fn);
   },
   onOpen(_property, fn = noop) {
-    new Function(`this.${_property} = true;`).call(this);
+    handleEvent.call(this, _property, true);
     return callback(fn);
   },
   onClose(_property, fn = noop) {
-    new Function(`this.${_property} = false;`).call(this);
+    handleEvent.call(this, _property, false);
     return callback(fn);
   },
   onShow(_property, fn = noop) {
-    new Function(`this.${_property} = true;`).call(this);
+    handleEvent.call(this, _property, true);
     return callback(fn);
   },
   onHide(_property, fn = noop) {
-    new Function(`this.${_property} = false;`).call(this);
+    handleEvent.call(this, _property, false);
     return callback(fn);
   }
 };

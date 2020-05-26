e6b460e02b7824f3ac6cf25ec4bc942c383d9fa2
Elf-mousE <ifmouz@gmail.com>
2020-05-26 12:40:36 +0800

    update

--- config/balmrc.js
+++ config/balmrc.js
@@ -97,7 +97,7 @@ module.exports = {
   pwa: {
     enabled: env.buildDocs,
     mode: 'injectManifest',
-    version: `v${version.replace(/\./g, '')}-2`
+    version: `v${version.replace(/\./g, '')}-3`
   },
   useDefaults: env.useDefault,
   logs: {

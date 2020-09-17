5bf727bb27b27e949ee0a8c9b8a1ebe4b3dbffa7
Elf-mousE <ifmouz@gmail.com>
2020-09-17 16:42:48 +0800

    update

--- docs/scripts/views/test.vue
+++ docs/scripts/views/test.vue
@@ -1,16 +1,62 @@
 <template>
-  <div>
-    Hello BalmUI
-    <ui-button @click="onTest">Test</ui-button>
+  <div class="page--top-app-bar">
+    {{ a.b.c }}
+    <ui-top-app-bar content-selector="#content-main" :type="type" :title="title" @nav="onTest">
+      <template #toolbar="{ toolbarItemClass }">
+        <ui-icon-button :class="toolbarItemClass" icon="file_download"></ui-icon-button>
+        <ui-icon-button :class="toolbarItemClass" icon="print"></ui-icon-button>
+        <ui-icon-button :class="toolbarItemClass" icon="bookmark"></ui-icon-button>
+      </template>
+    </ui-top-app-bar>
+
+    <ui-drawer v-model="a.b.c" type="modal">
+      <ui-drawer-header>
+        <ui-drawer-title>Header here</ui-drawer-title>
+      </ui-drawer-header>
+      <ui-drawer-content>
+        <ui-list>
+          <ui-item active @click="$router.back()">
+            <ui-item-first-content>
+              <ui-icon>arrow_back</ui-icon>
+            </ui-item-first-content>
+            <ui-item-text-content>Back</ui-item-text-content>
+          </ui-item>
+          <ui-list-divider></ui-list-divider>
+        </ui-list>
+      </ui-drawer-content>
+    </ui-drawer>
+
+    <ui-drawer-backdrop></ui-drawer-backdrop>
+
+    <div id="content-main">
+      <p v-for="i in 36" :key="i">Content</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { getCurrentInstance } from 'vue';
+
 export default {
   name: 'Test',
+  data() {
+    return {
+      type: 1,
+      title: 'Hello BalmUI',
+      openDrawer: false,
+      a: {
+        b: {
+          c: false
+        }
+      }
+    };
+  },
+  mounted() {
+    console.log('mounted', this.$balmUI);
+  },
   methods: {
-    onTest(e) {
-      console.log(e);
+    onTest() {
+      this.$balmUI.onShow('a.b.c');
     }
   }
 };

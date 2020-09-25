e8aadb06813dbe63cba90d543238d9479bff3526
Elf-mousE <ifmouz@gmail.com>
2020-09-25 18:11:45 +0800

    update

--- src/scripts/components/textfield.js
+++ src/scripts/components/textfield.js
@@ -1,10 +1,8 @@
 import multiBootstrap from '../config/multi-bootstrap';
 import UiTextfield from './input-controls/textfield';
-import UiTextfieldHelper from './input-controls/textfield-helper';
 import UiTextfieldIcon from './input-controls/textfield-icon';
 
 export default multiBootstrap({
   UiTextfield,
-  UiTextfieldHelper,
   UiTextfieldIcon
 });

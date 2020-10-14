bd1a20370aabf681714f949404c0aa2bb52fc4a2
Elf-mousE <ifmouz@gmail.com>
2020-10-14 10:32:04 +0800

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,13 +2,13 @@
 
 npm run prod
 
-LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo -e "Latest version: ${LatestTag}"
+# LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+# echo -e "Latest version: ${LatestTag}"
 
-echo -e "Input new release version: (e.g. v1.0.0)"
-read tagName
+# echo -e "Input new release version: (e.g. v1.0.0)"
+# read tagName
 
-git tag ${tagName}
-git push origin ${tagName}
+# git tag ${tagName}
+# git push origin ${tagName}
 
 npm publish --tag next

1731143e1e3a45890fd9abe1a23153689a1add33
Elf-mousE <ifmouz@gmail.com>
2020-10-23 10:31:37 +0800

    chore: update direction format

--- src/scripts/components/editor/formats/index.js
+++ src/scripts/components/editor/formats/index.js
@@ -1,10 +1,13 @@
 import useTextAlign from './align';
+import useDirection from './direction';
 import useFontFamliy from './font';
 import useFontSize from './size';
 import useLineHeight from './line-height';
 
 function useTypography(Quill, { font, size, lineheight }) {
   useTextAlign(Quill);
+  useDirection(Quill);
+
   font && useFontFamliy(Quill, font);
   size && useFontSize(Quill, size);
   lineheight && useLineHeight(Quill, lineheight);

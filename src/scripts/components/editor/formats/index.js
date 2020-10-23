219d1750eaa1ab39716546a77104bc411f395f3f
应用组-黄扬扬 <huangyangyang@yiban.cn>
2020-10-23 11:35:45 +0800

    update

--- src/scripts/components/editor/formats/index.js
+++ src/scripts/components/editor/formats/index.js
@@ -2,12 +2,14 @@ import useTextAlign from './align';
 import useFontFamliy from './font';
 import useFontSize from './size';
 import useLineHeight from './line-height';
+import useTextIndent from './text-indent';
 
-function useTypography(Quill, { font, size, lineheight }) {
+function useTypography(Quill, { font, size, lineheight, textindent }) {
   useTextAlign(Quill);
   font && useFontFamliy(Quill, font);
   size && useFontSize(Quill, size);
   lineheight && useLineHeight(Quill, lineheight);
+  textindent && useTextIndent(Quill)
 }
 
 export default useTypography;

599e27e814969fe21c9a1679d1ed55d661e10bef
Elf-mousE <ifmouz@gmail.com>
2020-11-04 11:08:23 +0800

    chore: update config

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,10 +5,30 @@ npm run prod
 LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo -e "Latest version: ${LatestTag}"
 
-echo -e "Input new release version: (e.g. v1.0.0)"
+echo -e "Input new release version: (e.g. 1.0.0)"
 read tagName
 
-git tag ${tagName}
-git push origin ${tagName}
+regVersion='^([0-9]+\.[0-9]+\.[0-9]+)$'
+regTag='^v([0-9]+\.[0-9]+\.[0-9]+)$'
 
-npm publish
+canRelease=false
+
+if [[ "$tagName" =~ $regTag ]]; then
+  canRelease=true
+elif [[ "$tagName" =~ $regVersion ]] && [[ ! "$tagName" =~ $regTag ]]; then
+  tagName="v${tagName}"
+  canRelease=true
+else
+  echo "Invalid version: ${tagName}"
+fi
+
+if $canRelease; then
+  echo "${tagName} releasing"
+
+  git tag ${tagName}
+  git push origin ${tagName}
+
+  npm publish
+
+  echo "${tagName} released"
+fi

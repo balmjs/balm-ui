d195eefb7edf879564112a121d9666feab1cbad9
elf-mouse <ifmouz@gmail.com>
2021-01-22 10:33:44 +0800

    chore: update

---
+++ test/components/fab.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils';
+import UiFab from '@/components/buttons/fab.vue';
+
+describe('<ui-fab>', () => {
+  it('renders default fab', () => {
+    const wrapper = shallowMount(UiFab);
+
+    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper.element.tagName).toBe('BUTTON');
+    expect(wrapper.classes()).toContain('mdc-fab');
+  });
+
+  it('renders extended fab', () => {
+    const wrapper = shallowMount(UiFab, {
+      propsData: {
+        extended: true
+      }
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper.classes()).toContain('mdc-fab--extended');
+  });
+});

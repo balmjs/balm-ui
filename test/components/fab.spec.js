e6be5c89289d661193a4c773e0e3fe688f57aa7b
elf-mouse <ifmouz@gmail.com>
2021-01-25 12:11:21 +0800

    test: update

--- test/components/fab.spec.js
+++ test/components/fab.spec.js
@@ -1,23 +1,24 @@
-import { shallowMount } from '@vue/test-utils';
+import { config, mount } from '@vue/test-utils';
 import UiFab from '@/components/buttons/fab.vue';
+
+config.stubRenderDefaultSlot = true;
 
 describe('<ui-fab>', () => {
   it('renders default fab', () => {
-    const wrapper = shallowMount(UiFab);
+    const wrapper = mount(UiFab);
 
-    expect(wrapper.element).toMatchSnapshot();
-    expect(wrapper.element.tagName).toBe('BUTTON');
-    expect(wrapper.classes()).toContain('mdc-fab');
+    expect(wrapper.get('button').element).toMatchSnapshot();
+    expect(wrapper.get('button').classes()).toContain('mdc-fab');
   });
 
   it('renders extended fab', () => {
-    const wrapper = shallowMount(UiFab, {
-      propsData: {
+    const wrapper = mount(UiFab, {
+      props: {
         extended: true
       }
     });
 
-    expect(wrapper.element).toMatchSnapshot();
-    expect(wrapper.classes()).toContain('mdc-fab--extended');
+    expect(wrapper.get('button').element).toMatchSnapshot();
+    expect(wrapper.get('button').classes()).toContain('mdc-fab--extended');
   });
 });

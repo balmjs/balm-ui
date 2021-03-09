001439160f123530dca061666a38d09663c53b57
elf-mouse <ifmouz@gmail.com>
2021-03-09 11:33:12 +0800

    test: update examples

--- examples/my-vite-app/vite.config.js
+++ examples/my-vite-app/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+function resolve(dir) {
+  return path.join(__dirname, '..', '..', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +13,9 @@ export default defineConfig({
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
       'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
-      'balm-ui-css': 'balm-ui/dist/balm-ui.css'
+      'balm-ui-css': 'balm-ui/dist/balm-ui.css',
+      'balm-ui-source': resolve('src/scripts'),
+      'balm-ui-plus-source': resolve('src/scripts/plus.js')
     }
   }
 });

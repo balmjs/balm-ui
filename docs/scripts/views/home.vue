d6c8e178a1421f11f69ca5f1d4541d95cd7d7273
elf-mouse <ifmouz@gmail.com>
2021-03-18 12:17:50 +0800

    docs: update home page lang

--- docs/scripts/views/home.vue
+++ docs/scripts/views/home.vue
@@ -4,15 +4,10 @@
       <h1 :class="$tt('headline1')">BalmUI</h1>
     </section>
     <div :class="[$tt('body2'), 'pure-docs']">
-      <p :class="$tt('subtitle1')">
-        Build beautiful products, faster for Vue.js
-      </p>
-      <p :class="$tt('subtitle2')">
-        BalmUI is a Google Material design system – backed by open-source code –
-        that helps teams build high-quality digital experiences.
-      </p>
+      <p :class="$tt('subtitle1')">{{ $t('home.slogan') }}</p>
+      <p :class="$tt('subtitle2')">{{ $t('home.title') }}</p>
       <ui-button raised @click="$router.push({ name: 'guide.intro' })">
-        Get started
+        {{ $t('home.get-started') }}
       </ui-button>
     </div>
   </div>

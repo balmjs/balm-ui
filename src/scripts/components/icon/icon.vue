b99f158b751de48b9392e9cae08d58cd14bb89a4
elf-mouse <ifmouz@gmail.com>
2021-03-18 17:57:09 +0800

    feat: update icon component

--- src/scripts/components/icon/icon.vue
+++ src/scripts/components/icon/icon.vue
@@ -69,6 +69,12 @@ export default {
     isSharp() {
       return this.checkType(UI_ICON.TYPES, 'sharp');
     },
+    invalidIcon() {
+      return this.dark && this.light;
+    },
+    activeIcon() {
+      return this.dark || this.light;
+    },
     className() {
       let result = {
         'material-icons': this.isFilled,
@@ -88,9 +94,16 @@ export default {
       return result;
     }
   },
+  created() {
+    if (this.invalidIcon || (!this.activeIcon && this.inactive)) {
+      console.warn('[BalmUI icon]: Invalid dark or light icon.');
+    }
+  },
   methods: {
     handleClick(event) {
-      this.$emit(UI_ICON.EVENT.CLICK, event);
+      if (!this.inactive) {
+        this.$emit(UI_ICON.EVENT.CLICK, event);
+      }
     }
   }
 };

8c7da4cababb6abb497f06e4e110c3e3dce78f44
elf-mouse <ifmouz@gmail.com>
2021-03-31 15:28:13 +0800

    fix: overwirte components props bug

--- src/scripts/config/multi-configure.js
+++ src/scripts/config/multi-configure.js
@@ -2,14 +2,12 @@ import configure from './configure';
 
 const multiConfigure = (Components, options) => {
   for (const key of Object.keys(options)) {
-    if (Components[key] === undefined) {
-      return;
+    if (/^Ui[A-Z]{1}[A-Za-z]+$/.test(key)) {
+      const Component = Components[key];
+      const props = options[key];
+
+      configure(Component, props);
     }
-
-    const Component = Components[key];
-    const props = options[key];
-
-    configure(Component, props);
   }
 };
 

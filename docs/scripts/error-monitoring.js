dcbde2100a56c945a119974f708e15d5ca31c723
elf-mouse <ifmouz@gmail.com>
2021-12-24 13:12:24 +0800

    docs: update

--- docs/scripts/error-monitoring.js
+++ docs/scripts/error-monitoring.js
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
+import { VERSION } from '@/config';
 
 function errorMonitoring(app, router) {
   Sentry.init({
     app,
     dsn: 'https://efed44e0fdc94f21917e3e0a1d9d7d9b@o1097421.ingest.sentry.io/6118851',
+    release: `balm-ui@${VERSION}`,
     integrations: [
       new Integrations.BrowserTracing({
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),

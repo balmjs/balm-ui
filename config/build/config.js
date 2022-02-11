2dc62190ebed2880914140057842900f23e789fc
elf-mouse <ifmouz@gmail.com>
2022-02-11 16:38:50 +0800

    chore: update vetur

--- config/build/config.js
+++ config/build/config.js
@@ -103,7 +103,22 @@ const noAttributesTags = [
   'ui-drawer-app-content',
   'ui-panel',
   'ui-menuitem-text',
-  'ui-menuitem-icon'
+  'ui-menuitem-icon',
+  'ui-item-text-content',
+  'ui-item-text1',
+  'ui-item-text2',
+  'ui-item-first-content',
+  'ui-item-last-content',
+  'ui-list-group',
+  'ui-list-group-subheader',
+  'ui-image-text',
+  'ui-card-content',
+  'ui-card-media-content',
+  'ui-card-text',
+  'ui-card-buttons',
+  'ui-card-icons',
+  'ui-dialog-content',
+  'ui-tooltip-anchor'
 ];
 
 module.exports = {

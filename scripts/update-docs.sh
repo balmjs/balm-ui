4d237c20812bbabb64d6eb593cc0bfb297c5fa37
elf-mouse <ifmouz@gmail.com>
2022-02-15 11:56:02 +0800

    chore: update

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+DistTags=$(npm view balm-ui dist-tags --json)
+LatestVersion=$(echo "$DistTags" | jq --raw-output '.latest')
+
 npm run docs
 cd dist
 git init
 git add -A
-git commit -m 'update official site for balm-ui'
-git push -f git@github.com:balmjs/material.balmjs.com.git main:gh-pages
+git commit -m "update official site for balm-ui@$LatestVersion"
+git push -f git@github.com:balmjs/material.balmjs.com.git master:gh-pages

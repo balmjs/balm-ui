877388c8a6de70042a8b9f27018e661952835e79
elf-mouse <ifmouz@gmail.com>
2022-03-03 17:26:40 +0800

    chore: update

--- docs/scripts/views/test/composition.vue
+++ docs/scripts/views/test/composition.vue
@@ -1,11 +1,20 @@
 <template>
   <div ref="root">Composition API</div>
+  <ui-textfield v-model="value" required>label</ui-textfield>
+  <ui-select v-model="value2" :options="options" required>label</ui-select>
 </template>
 
 <script setup>
 import { ref, reactive, toRefs, onMounted } from 'vue';
 
 const root = ref(null);
+
+const state = reactive({
+  value: '',
+  value2: '',
+  options: []
+});
+const { value, value2, options } = toRefs(state);
 
 onMounted(() => {
   console.log(root.value);

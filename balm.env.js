e34191d2148977f7c98fc334ca5464c28c07dea1
Elf-mousE <ifmouz@gmail.com>
2022-04-27 08:01:44 +0800

    chore: update

--- balm.env.js
+++ balm.env.js
@@ -1,5 +1,7 @@
-const BALM_ROOT = '/Users/elf-mouse/www/balmjs/balm-next';
+process.env.PUBLIC_URL = 'https://material.balmjs.com';
 
+// const BALM_ROOT = '/Users/elf-mouse/www/balmjs/balm-next';
 // process.env.BALM_CORE = `${BALM_ROOT}/packages/balm-core`;
 
-process.env.PUBLIC_URL = 'https://material.balmjs.com';
+process.env.BALM_COR =
+  '/Users/elf-mouse/.config/yarn/global/node_modules/balm-core';

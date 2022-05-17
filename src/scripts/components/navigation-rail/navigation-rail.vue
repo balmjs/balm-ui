8f0cb5bb675b748ee800192e3b9d6d96548904e4
Elf-mousE <ifmouz@gmail.com>
2022-05-17 09:26:08 +0800

    chore: add navigation rail

--- src/scripts/components/navigation-rail/navigation-rail.vue
+++ src/scripts/components/navigation-rail/navigation-rail.vue
@@ -1,11 +1,37 @@
 <template>
   <!-- (M3) Container -->
   <div class="mdc-navigation-rail">
-    <slot></slot>
+    <!-- Menu (optional) -->
+    <div class="mdc-navigation-rail__menu">
+      <slot name="menu">
+        <mdc-icon-button
+          v-if="hasMenu"
+          class="mdc-navigation-rail__navigation-icon"
+        >
+          menu
+        </mdc-icon-button>
+      </slot>
+    </div>
+
+    <!-- Floating action button (optional) -->
+    <div class="mdc-navigation-rail__key-action">
+      <slot name="key-action"></slot>
+    </div>
+
+    <!-- Primary destinations (3 to 7) -->
+    <div class="mdc-navigation-rail__destinations">
+      <slot>
+        <ul v-if="items.length">
+          <li v-for="(item, index) in items" :key="index">{{ items }}</li>
+        </ul>
+      </slot>
+    </div>
   </div>
 </template>
 
 <script>
+// Define navigation rail constants
+
 export default {
   name: 'UiNavigationRail',
   customOptions: {}
@@ -14,4 +40,10 @@ export default {
 
 <script setup>
 // TODO
+const props = defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  }
+});
 </script>

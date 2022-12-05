62e042979fc90134595ddaa94e8f7ff60277169a
elf-mouse <ifmouz@gmail.com>
2022-12-05 13:53:37 +0800

    chore: update config

--- config/balmrc.js
+++ config/balmrc.js
@@ -19,7 +19,7 @@ function getConfig(balm) {
     ? {
         modules: false,
         useBuiltIns: 'entry',
-        corejs: { version: '3.24' }
+        corejs: { version: '3.26' }
       }
     : {
         modules: false

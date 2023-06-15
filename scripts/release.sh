6f7e52044cb42d3fa9339232e8384c21cd8a3bbe
elf-mouse <ifmouz@gmail.com>
2023-06-15 16:15:30 +0800

    chore: update config

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,27 +5,31 @@ npm run prod
 LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo -e "Latest version: ${LatestTag}"
 
-echo -e "Input new release version: (e.g. 10.0.0)"
+echo -e "Input new release version: (e.g. 11.0.0)"
 read tagName
 
-regVersion='^(10\.[0-9]+\.[0-9]+)$'
-regTag='^v(10\.[0-9]+\.[0-9]+)$'
+regVersion='^(11\.[0-9]+\.[0-9]+)$'
+regTag='^v(11\.[0-9]+\.[0-9]+)$'
 
-canRelease=false
+# canRelease=false
 
-if [[ "$tagName" =~ $regTag ]]; then
-  canRelease=true
-elif [[ "$tagName" =~ $regVersion ]] && [[ ! "$tagName" =~ $regTag ]]; then
-  tagName="v${tagName}"
-  canRelease=true
-else
-  echo "Invalid version: ${tagName}"
-fi
+# if [[ "$tagName" =~ $regTag ]]; then
+#   canRelease=true
+# elif [[ "$tagName" =~ $regVersion ]] && [[ ! "$tagName" =~ $regTag ]]; then
+#   tagName="v${tagName}"
+#   canRelease=true
+# else
+#   echo "Invalid version: ${tagName}"
+# fi
+
+# for canary
+tagName="v11.0.0-canary.${tagName}"
+canRelease=true
 
 if $canRelease; then
   echo "${tagName} releasing"
 
-  npm publish
+  npm publish --tag canary
 
   git tag ${tagName}
   git push origin ${tagName}
